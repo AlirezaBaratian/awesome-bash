d2d31a80312d72f494d255af48b2ac19a18f35d9
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-20 17:07:16 +0330

    fix: Fix variable interpolation

--- create_user.sh
+++ create_user.sh
@@ -7,5 +7,5 @@ sleep 2
 read -p "Enter the user's name: " name
 read -p "Enter a strong password for this user: " password
 
-useradd -m name -G sudo -s /bin/bash
+useradd -m $name -G sudo -s /bin/bash
 echo "$passwrod\n$passwrod" | passwd $name
