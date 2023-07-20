da8cca1eedee8c8f88639897ba6e6a258b3a892b
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-20 17:05:40 +0330

    fix: Fix variable input syntax

--- create_user.sh
+++ create_user.sh
@@ -4,8 +4,8 @@ clear
 echo "This script creates a sudo user with your given name and password."
 sleep 2
 
-read -p "Enter the user's name: " $name
-read -p "Enter a strong password for this user: " $password
+read -p "Enter the user's name: " name
+read -p "Enter a strong password for this user: " password
 
 useradd -m name -G sudo -s /bin/bash
-echo "$passwrod\n$passwrod" | passwd name
+echo "$passwrod\n$passwrod" | passwd $name
