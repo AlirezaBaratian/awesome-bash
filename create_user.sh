6d72ee7a59d855b92676a99c5e66cc9b9062b7e1
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-20 17:00:35 +0330

    feat: Add create_user

---
+++ create_user.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+clear
+echo "This script creates a sudo user with your given name and password."
+sleep 2
+
+read -p "Enter the user's name: " $name
+read -p "Enter a strong password for this user: " $password
+
+useradd -m name -G sudo -s /bin/bash
+echo "$passwrod" | passwd name
