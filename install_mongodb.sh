d428897077b0a76650f62da686a0a37cdc681e49
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-20 20:40:45 +0330

    feat: Add install_mongodb

---
+++ install_mongodb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+clear
+echo "Installing mongodb 6 on your server..."
+
+sudo apt-get install gnupg curl
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-5.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo systemctl start mongod
+sudo systemctl status mongod
+sudo systemctl enable mongod
