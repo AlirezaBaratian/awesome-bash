0c7e42c1e90f500df9548e4417487c700b781d93
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-22 15:39:34 +0330

    feat: Add install_speedtest_debian

---
+++ install_speedtest_debian.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Installing speedtest for your debian-based machine..."
+sleep 1
+
+## If migrating from prior bintray install instructions please first...
+# sudo rm /etc/apt/sources.list.d/speedtest.list
+# sudo apt-get update
+# sudo apt-get remove speedtest
+## Other non-official binaries will conflict with Speedtest CLI
+# Example how to remove using apt-get
+# sudo apt-get remove speedtest-cli
+sudo apt-get install curl
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+sudo apt-get install speedtest
