b4b947ceaf543c3d0f6333e14277b5c0a8417c0b
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-24 20:14:55 +0330

    feat: Add install_mongodb

---
+++ install_mongodb.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+clear
+echo "This script installs and configures MongoDB 6.0 on your 22.04 Ubuntu..."
+sleep 2
+
+sudo apt-get install gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+   --dearmor
+
+# Create the /etc/apt/sources.list.d/mongodb-org-6.0.list file for Ubuntu 22.04 (Jammy):
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo systemctl start mongod
+sudo systemctl status mongod
+sudo systemctl enable mongod
