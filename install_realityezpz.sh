32e501b1bb50e22680f789a2e8cc4baccd188911
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-27 16:47:09 +0330

    fix: Fix minor file name issue

--- install_realityezpz.sh
+++ install_realityezpz.sh
@@ -4,8 +4,7 @@ echo "Installing REALITY-EZPZ..."
 sleep 1
 
 git clone https://github.com/aleskxyz/reality-ezpz.git
-sleep 3
 
 cd reality-ezpz
 chmod +x reality-ezpz.sh
-./reality-ezpz
+./reality-ezpz.sh
