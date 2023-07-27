4673f698c8162a4fd540faf73127163c5260ca13
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-07-27 16:51:23 +0330

    feat: Install to /tmp and remove after done

--- install_realityezpz.sh
+++ install_realityezpz.sh
@@ -3,8 +3,10 @@
 echo "Installing REALITY-EZPZ..."
 sleep 1
 
-git clone https://github.com/aleskxyz/reality-ezpz.git
+git clone https://github.com/aleskxyz/reality-ezpz.git /tmp/reality-ezpz/
 
-cd reality-ezpz
+cd /tmp/reality-ezpz/
 chmod +x reality-ezpz.sh
 ./reality-ezpz.sh
+
+rm -rf /tmp/reality-ezpz/
