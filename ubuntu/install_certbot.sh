872f34e3362c474ae3174fec30a1fbd56329b244
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-08-13 16:46:09 +0330

    fix: Edit package name

--- ubuntu/install_certbot.sh
+++ ubuntu/install_certbot.sh
@@ -4,7 +4,7 @@ clear
 echo "🤖 Installing certbot via apt on your Ubuntu machine..."
 
 sudo apt update
-sudo apt install -y certbot python3-certbot
+sudo apt install -y certbot python3-certbot-nginx
 
 clear
 echo "✅ Successfully installed certbot, use it with this command:
