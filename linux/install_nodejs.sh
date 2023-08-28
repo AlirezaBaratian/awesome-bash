33d0aacb727e0641ff9f725b3281b22e809009e2
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-08-28 16:21:44 +0330

    fix: Run install script

--- linux/install_nodejs.sh
+++ linux/install_nodejs.sh
@@ -3,16 +3,17 @@
 clear
 echo "✂️ Installing nodejs v16.18.1 with nvm tool for future version management"
 
-VERSION = "v16.18.1"
+version = "v16.18.1"
 # Download and install nvm
 curl -L -o /tmp/nvm-install.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh
+bash /tmp/nvm-install.sh
 
 # Add nvm to path
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Install nodejs v16.18.1
-nvm install $VERSION
+nvm install $version
 
 clear
 echo "✅ Installed nodejs $VERSION"
