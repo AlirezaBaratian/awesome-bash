7769c064d84c87bdb36c5fa298f0feae0e0ef08a
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-08-28 16:18:18 +0330

    feat: Add install_nodejs.sh

---
+++ linux/install_nodejs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+clear
+echo "✂️ Installing nodejs v16.18.1 with nvm tool for future version management"
+
+VERSION = "v16.18.1"
+# Download and install nvm
+curl -L -o /tmp/nvm-install.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh
+
+# Add nvm to path
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Install nodejs v16.18.1
+nvm install $VERSION
+
+clear
+echo "✅ Installed nodejs $VERSION"
