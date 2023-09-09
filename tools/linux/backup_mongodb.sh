1ede546663c180dbf21529791b530e089b34d3f6
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-09-09 21:07:07 +0330

    fix: Create archive in the correct dir

--- tools/linux/backup_mongodb.sh
+++ tools/linux/backup_mongodb.sh
@@ -8,7 +8,7 @@ backup_filename="mongodb_backup_$(date +%Y%m%d).tar.xz"
 
 mkdir -p $backup_dir
 mongodump --out "$backup_dir/dump"
-tar -cvJf "$backup_filename" "$backup_dir/dump"
+tar -cvJf "$backup_dir/$backup_filename" "$backup_dir/dump"
 rm -rf "$backup_dir/dump"
 
 echo "Done ✅"
