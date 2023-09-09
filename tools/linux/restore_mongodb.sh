bb735eef3417d6f5d8311ca646d967deac0be6b8
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-09-09 21:32:05 +0330

    fix: Edit dir switch

--- tools/linux/restore_mongodb.sh
+++ tools/linux/restore_mongodb.sh
@@ -5,5 +5,5 @@ read -p "👉🏼 Enter the absolute path of the backup file: " path
 extract_dir= "/tmp/mogndob_tmp_backup_extract"
 
 mkdir -p $extract_dir
-tar xf $path -C $extract_dir
+tar xf $path --dir $extract_dir
 mongorestore "$extract_dir/dump"
