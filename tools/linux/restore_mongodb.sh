170495e1f6c6c44cea491ec0637a627b9d8ab215
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-09-09 21:43:14 +0330

    fix: Fix mongorestore

--- tools/linux/restore_mongodb.sh
+++ tools/linux/restore_mongodb.sh
@@ -3,7 +3,8 @@
 clear
 read -p "👉🏼 Enter the absolute path of the backup file: " path
 extract_dir="/tmp/mogndob_tmp_backup_extract"
+backup_dir="/var/lib/mongodb_backup"
 
 mkdir -p $extract_dir
 tar xvJf $path -C $extract_dir
-mongorestore "$extract_dir/dump"
+mongorestore "$extract_dir/$backup_dir/dump"
