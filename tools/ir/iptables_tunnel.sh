bdf12f07efbebea67deb6b7717ff335482e800f3
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-09-11 13:22:16 +0330

    fix: Remove wrong package

--- tools/ir/iptables_tunnel.sh
+++ tools/ir/iptables_tunnel.sh
@@ -6,7 +6,7 @@ read -p "Enter your foreign server IP address: " ip_address
 read -p "Enter your desired port: " port
 
 sudo apt update
-sudo apt install -y sysctl iptables
+sudo apt install -y iptables
 
 sudo sysctl net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -j MASQUERADE
