58a5740c13ef9d72ab94221d351986b6cc729bf8
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-09-11 13:15:34 +0330

    feat: Add iptables_tunnel.sh

---
+++ tools/ir/iptables_tunnel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+clear
+echo "IR to * Tunnel Script"
+read -p "Enter your foreign server IP address: " ip_address
+read -p "Enter your desired port: " port
+
+sudo apt update
+sudo apt install -y sysctl iptables
+
+sudo sysctl net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $ip_address:$port
