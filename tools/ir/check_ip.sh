e99d8ba625fd82b944524dbaace3c2a4a0f7c231
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-09-25 16:40:27 +0330

    feat: Add check_ip.sh to ir/

---
+++ tools/ir/check_ip.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+clear
+
+read -p "🌐 Enter the IP address: " ip_address
+read -p "🎲 How many times do you want to ping it? " count
+
+
+ping_result=$(ping -c "$count" "$ip_address")
+
+if [ $? -eq 0 ]; then
+  echo "✅ $ip_address is clean."
+  exit 0
+else
+  echo "❌ $ip_address is probably dirty since some packets were dropped."
+  exit
+fi
