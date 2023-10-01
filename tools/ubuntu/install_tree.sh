1eaacc0d38082e94219b42a380b8a2126ccd4b1b
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-10-01 12:27:13 +0330

    feat: Add install_tree.sh

---
+++ tools/ubuntu/install_tree.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+clear
+echo "🌲 Installing 'tree' utility on your machine..."
+sleep 2
+
+sudo apt update
+sudo apt install -y tree
+
+echo "✅ Successfully installed 'tree' on your machine."
