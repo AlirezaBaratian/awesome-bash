70c0ad6a94691a9035c61b221464ec18ffe6058d
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-10-08 19:07:49 +0330

    fix: Add -y switch and sed comment

--- tools/ubuntu/marzban/switch_xray_core.sh
+++ tools/ubuntu/marzban/switch_xray_core.sh
@@ -5,7 +5,7 @@ echo "This script switched Xray core to the latest version."
 sleep 2
 
 # Install dependencies
-apt install wget unzip
+apt install -y wget unzip
 
 # Downlaod and setup Xray core inside marzban data directory
 download_url="https://github.com/XTLS/Xray-core/releases/download/v1.8.4/Xray-linux-64.zip"
@@ -19,7 +19,7 @@ rm Xray-linux-64.zip
 
 # Switch Xray core in environemt variables
 env_path="/opt/marzban/.env"
-search_line="XRAY_EXECUTABLE_PATH ="
+search_line="# XRAY_EXECUTABLE_PATH ="
 new_line='XRAY_EXECUTABLE_PATH = "/var/lib/marzban/xray-core/xray"'
 
 sed -i "s/$search_line/$new_line/g" "$env_path"
