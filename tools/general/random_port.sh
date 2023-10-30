7a20c61687856d9cd261dc76cc53fd46988e5acd
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-10-30 14:09:30 +0330

    feat: Add min and max port args

--- tools/general/random_port.sh
+++ tools/general/random_port.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+if [$$1 && $2]; then
+    min=$1
+    max=$2
+else
+    min=1024
+    max=64512
+fi
+
 # Generate a random number between 1024 and 65535
-random_port=$(( RANDOM % 64512 + 1024 ))
+random_port=$(( RANDOM % max + min ))
 
 echo "🌐 Random Port: $random_port"
