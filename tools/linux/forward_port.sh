b85e4fc8e0afb5474731b80e0e17a69bf6690790
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-11-07 16:26:54 +0330

    feat: Add forward_port.sh

---
+++ tools/linux/forward_port.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+raed -p "Enter your listening port: " listening_port
+raed -p "Enter your local port: " local_port
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport $listening_port -j REDIRECT --to-port $local_port
