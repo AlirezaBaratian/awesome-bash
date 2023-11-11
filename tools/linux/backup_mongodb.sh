74636478cd074fe148a1e4943ab20663fae2639b
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-11-11 12:44:28 +0330

    refactor: Rewrite as main - Fixed extra path

--- tools/linux/backup_mongodb.sh
+++ tools/linux/backup_mongodb.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-echo "We are about to backup current state of the MongoDB database"
-sleep 1
+function main() {
+    echo "We are about to backup current state of the MongoDB database... 😅"
+    sleep 1
 
-backup_dir="/var/lib/mongodb-backup"
-backup_filename="mongodb_backup_$(date +%Y%m%d).tar.xz"
+    backup_dir="/var/lib/mongodb-backup"
+    backup_filename="mongodb_backup_$(date +%Y%m%d).tar.xz"
 
-mkdir -p $backup_dir
-mongodump --out "$backup_dir/dump"
-tar -cvJf "$backup_dir/$backup_filename" "$backup_dir/dump"
-rm -rf "$backup_dir/dump"
+    mkdir -p $backup_dir
+    cd backup_dir
+    mongodump --out "dump"
+    tar -cvJf "$backup_filename" "dump"
+    rm -rf "$dump"
 
-echo "Done ✅"
+    echo "Done ✅"
+}
+
+main()
