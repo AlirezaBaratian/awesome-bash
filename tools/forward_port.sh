989d2c035782023c258b0f56e90b4d2d2fac7741
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-11-20 16:30:34 +0330

    fix: Add quotes

--- tools/forward_port.sh
+++ tools/forward_port.sh
@@ -4,7 +4,7 @@ function main() {
     raed -p "Choose a new port: " new_port
     raed -p "Enter the port you want to forward: " forward_port
 
-    sudo iptables -t nat -A PREROUTING -p tcp --dport $new_port -j REDIRECT --to-port $forward_port
+    sudo iptables -t nat -A PREROUTING -p tcp --dport "$new_port" -j REDIRECT --to-port "$forward_port"
 }
 
 main
