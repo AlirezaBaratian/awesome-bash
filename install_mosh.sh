848db6976d44f93ceeac7a23a811649467e78b6a
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2024-01-07 13:01:41 +0330

    fix: remove extras

--- install_mosh.sh
+++ install_mosh.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 function main() {
-    echo "We're going to install mosh..."
-    sleep 3
     sudo apt update
     sudo apt install -y mosh
-
-    echo "✅ Successfully installed mosh on your machie."
 }
 
 main
